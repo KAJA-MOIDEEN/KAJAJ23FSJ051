1dc21ae98ee246555ccb290bb1dd2331ed7077d5
KAJA MOIDEEN MK || காஜா மொய்தீன் MK <114286389+KAJA-MOIDEEN@users.noreply.github.com>
2023-07-07 00:38:01 +0530

    4th commit

---
+++ src/BasicOpen_LabBook/PyramidExampleReverse.java
@@ -0,0 +1,14 @@
+package BasicOpen_LabBook;
+
+public class PyramidExampleReverse {
+	public static void main(String[] args) {
+		int temp = 6 ;
+		for(int i = 1;i<=temp;i++) {
+			for(int j =temp;j>=i;j--) {
+				System.out.print("* ");
+			}
+			System.out.println();// new line
+		}
+	}
+
+}
