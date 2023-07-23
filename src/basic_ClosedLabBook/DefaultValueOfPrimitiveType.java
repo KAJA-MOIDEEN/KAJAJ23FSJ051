171beb1ba7d6885b4d3e17663207db739b0f434f
KAJA MOIDEEN MK || காஜா மொய்தீன் MK <114286389+KAJA-MOIDEEN@users.noreply.github.com>
2023-07-23 14:10:21 +0530

    Add files via upload

---
+++ src/basic_ClosedLabBook/DefaultValueOfPrimitiveType.java
@@ -0,0 +1,24 @@
+package basic_ClosedLabBook;
+
+public class DefaultValueOfPrimitiveType {
+	
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static boolean bool;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("default value of byte is : " + b);
+		System.out.println("default value of short is : " + s);
+		System.out.println("default value of int is : " + i);
+		System.out.println("default value of long is : " + l);
+		System.out.println("default value of float is : " + f);
+		System.out.println("default value of double is : " + d);
+		System.out.println("default value of boolean is : " + bool);
+	}
+
+}
