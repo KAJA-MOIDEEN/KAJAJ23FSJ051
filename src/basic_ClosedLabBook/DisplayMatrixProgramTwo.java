69ef9c91f1be3813033cb254510e9f5319db29ba
KAJA MOIDEEN MK || காஜா மொய்தீன் MK <114286389+KAJA-MOIDEEN@users.noreply.github.com>
2023-08-07 02:06:56 +0530

    Add files via upload

---
+++ src/basic_ClosedLabBook/DisplayMatrixProgramTwo.java
@@ -0,0 +1,22 @@
+package basic_ClosedLabBook;
+/*11.Write a program to display number matrix as follows using Two Dimensional
+Rectangular Array.
+1
+2 3
+4 5 6
+7 8 9 10*/
+public class DisplayMatrixProgramTwo {
+	public static void main(String[] args) {
+	int row = 4 , col = 4 ,num=1;
+		int[][] arr2D = new int[row][col];
+		for(int i = 0;i<row;i++) {
+			for(int j = 0;j<=i;j++) {
+				arr2D[i][j] = num;
+				num++;
+				System.out.print(arr2D[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
