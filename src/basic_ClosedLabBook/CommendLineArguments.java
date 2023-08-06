cb54bed6cf47bc1d6cc0c8525499f7390a2d9c5a
KAJA MOIDEEN MK || காஜா மொய்தீன் MK <114286389+KAJA-MOIDEEN@users.noreply.github.com>
2023-08-07 02:27:18 +0530

    Add files via upload

---
+++ src/basic_ClosedLabBook/CommendLineArguments.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package basic_ClosedLabBook;
+
+/**
+ * 
+ */
+public class CommendLineArguments {
+
+	public static void main(String[] args) {
+		 if(args.length == 0) {
+			 System.out.println("NO COMMAND LINE ARGUMENTS POVIDED.");
+		 }
+		 else {
+			 System.out.println("COMMAND LINE ARGUMENTS ");
+		 }
+		 for(int i=0;i<args.length;i++) {
+			  System.out.println("Argument" +(i + 1)+":"+args[i]);
+		 }
+
+	}
+
+}
