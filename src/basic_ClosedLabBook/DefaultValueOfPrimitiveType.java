3313bf4eed2fc08d5838a05a842fc5492fdc9005
KAJA MOIDEEN MK || காஜா மொய்தீன் MK <114286389+KAJA-MOIDEEN@users.noreply.github.com>
2023-08-07 01:31:03 +0530

    Update DefaultValueOfPrimitiveType.java

--- src/basic_ClosedLabBook/DefaultValueOfPrimitiveType.java
+++ src/basic_ClosedLabBook/DefaultValueOfPrimitiveType.java
@@ -1,5 +1,7 @@
 package basic_ClosedLabBook;
-
+/*3. Write a program to declare all primitive data types with all possible types of
+initialization and also check implicit and explicit type casting by assigning
+them to each other.*/
 public class DefaultValueOfPrimitiveType {
 	
 	static byte b;
