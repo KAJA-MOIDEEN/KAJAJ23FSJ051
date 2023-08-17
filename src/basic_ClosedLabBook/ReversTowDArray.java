538c50d15e7f04f0895ecabeb7fd4af8f47841b4
ELCOT <ELCOT@Lenovo>
2023-08-17 20:11:18 +0530

    practice

--- src/basic_ClosedLabBook/ReversTowDArray.java
+++ src/basic_ClosedLabBook/ReversTowDArray.java
@@ -5,33 +5,31 @@ import java.util.Arrays;
 
 public class ReversTowDArray {
 
-    public static void main(String[] args) {
-        int[][] array = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12}
-        };
-        
-        reverseArrayRows(array);
-        
-        for (int[] row : array) {
-            System.out.println(Arrays.toString(row));
-        }
+	    public static void main(String[] args) {
+	 int[][] array = {
+			 {1,2,3,4},
+			 {5,6,7,8},
+			 {9,10,11,12}
+	 };
+	 reverseAnArray(array);
+	 
+	 for(int[] row : array) {
+		 System.out.println(Arrays.toString(row));
+	 }
     }
-    
-    public static void reverseArrayRows(int[][] array) {
-        for (int[] row : array) {
-            int left = 0;
-            int right = row.length - 1;
-            
-            while (left < right) {
-                int temp = row[left];
-                row[left] = row[right];
-                row[right] = temp;
-                
-                left++;
-                right--;
-            }
-        }
-    }
-}
+	    private static void reverseAnArray(int[][] array){
+	    	for(int[] row : array) {
+	    		int left = 0;
+	    		int right = row.length -1;
+	    		
+	    		while(left<right) {
+	    			int temp = row[left];
+	    			row[left] = row[right];
+	    			row[right] = temp;
+	    			left++;
+	    			right--;
+	    		}
+	    	}
+	    	
+	    }
+	    }
